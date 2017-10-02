0cc9e6aa957a4e8972586cc84cf3c4a308ad885f
Adi <adi.bolb@gmail.com>
2017-10-02 09:48:27 +0300

    Initial commit, junit setup

---
+++ src/RomanNumerals.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RomanNumerals {
+
+    @Test
+    public void something(){
+        assertEquals(1,2);
+    }
+}
