b0d203b75507e070cb1c67344952a7eba0eef5c6
Adi <adi.bolb@gmail.com>
2017-10-02 21:07:36 +0300

    Refactored code

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -33,6 +33,26 @@ public class RomanNumerals {
     // Talk in intention, then location, then details
     // Navigator will use the time while the Driver is doing something to
     // understand what to do next.
+    // It's impossible for one of us not to be talking
+    // Have the dialogue curtosey
+    // The navigator can program much better in a pair than alone, because
+    // not crashing between the low and the high level
+    // Two situations: when the driver knows what to do, and when driver doesn't know what to do
+    // You can always learn things while pairing
+    // The navigator can learn a lot from the driver
+    // It's OK to go down the wrong path
+    // Confidence increased with often commits and good unit tests
+    // Use comments when the intent is more complicated
+    // Always have the intent clearly in your head, in "English"
+    // The readability of the code is important
+    // Ask for "trust" on short term when you are navigator and you are doing
+    // weird things
+    // When someone doesn't trust you, it doesn't matter how many logic you use
+    // As a navigator in strong style time goes faster, because you are in a flow
+    // The navigator is more engaged in strong style pairing
+    // Domain knowledge increases dramatically when doing pairing
+
+
 
     @Test
     public void addingIAndIShouldResultInII(){
